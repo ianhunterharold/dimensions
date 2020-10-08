c1ee690239724589f5b9f73c0f12356e20636a5a
Ian Harold <ianhunterharold@gmail.com>
2020-10-08 13:39:13 -0600

    modifying images with dimensions

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { StyleSheet, Image, View,Dimensions } from "react-native";
+
+import picBiscuit from './assets/biscuit.jpg';
+import picJungle from './assets/jungle.jpg';
+
+export default function App() {
+  return (
+    <View style={styles.page}>
+      <Image style={styles.image} source={picBiscuit}/>
+      <Image style={styles.image} source={picJungle}/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  image: {
+    flex: 1,
+    borderRadius: 50,
+    margin: 10,
+    marginTop: 20,
+    marginBottom: 5,
+    width: Dimensions.get('window').width - 20
+  }
+});
